0b00192207372add53c1f0a41f24d41a13e99dce
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-11-24 02:40:12 -0500

    working on the home page

--- src/Components/PlantHome.jsx
+++ src/Components/PlantHome.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function PlantHome() {
   return (
     <div className="plant_home">
-      <h1>Plant Home Page</h1>
+      <div>
+        <h3>Announcement Bar</h3>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <h1>Image Text</h1>
+        <Link>Shop All Plants</Link>
+        <Link>Shop Accessories</Link>
+      </div>
+      <div>
+        <h2>Our Best-Selling Plants</h2>
+        
+      </div>
+      {/* <h1>Plant Home Page</h1> */}
     </div>
   );
 }
